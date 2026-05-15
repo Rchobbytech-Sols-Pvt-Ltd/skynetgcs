f1908dc127d11cea896c788e9d4ef23726ac0c8d
jhakrishan20 <krishanjha80@gmail.com>
2026-05-15 11:11:27 +0530

    modified latest verison ie 0.1.2

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,7 +9,7 @@ const (
 	GitHubRepo  = "skynetgcs"
 
 	AppName    = "SkynetGCS"
-	AppVersion = "0.1.0"
+	AppVersion = "0.1.2"
 )
 
 // Component declares one piece of the GCS bundle that lives as a separate
